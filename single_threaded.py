f56376b80472e05bc4e01a64a877e4800361062d
Alex <aslvrstn@gmail.com>
2022-05-31 21:45:03 -0400

    Run multiple generations and accumulate successes

--- single_threaded.py
+++ single_threaded.py
@@ -1,23 +1,50 @@
 import hashlib
+import time
 from random import randbytes
+from typing import Optional, List
 
-def mine():
-    target_len = 5
+
+def mine(target_len: int) -> Optional[bytes]:
     max_attempts = 1000000
     success = "0" * target_len
     attempts = 0
     while attempts < max_attempts:
-        h = hashlib.new('sha256')
+        h = hashlib.new("sha256")
         attempt = randbytes(100)
         h.update(attempt)
         hashed = h.hexdigest()
         if hashed[-target_len:] == success:
-            print(h.hexdigest())
             print(f"Took {attempts} attempts")
             return attempt
         attempts += 1
 
     return None
 
+
+def mine_multiple(successes_desired: int, target_len: int) -> List[bytes]:
+    successes = []
+    while len(successes) < successes_desired:
+        res = mine(target_len=target_len)
+        if res is not None:
+            successes.append(res)
+
+    return successes
+
+
 if __name__ == "__main__":
-    mine()
+    start = time.time()
+    target_len = 4
+
+    successes_desired = 100
+    successes = mine_multiple(successes_desired, target_len)
+
+    # Let's make sure our workers didn't misbehave!
+    success = "0" * target_len
+    for text in successes:
+        h = hashlib.new("sha256")
+        h.update(text)
+        hashed = h.hexdigest()
+        assert hashed[-target_len:] == success
+
+    end = time.time()
+    print(f"Generated {len(successes)} hashes in {end-start}s")
